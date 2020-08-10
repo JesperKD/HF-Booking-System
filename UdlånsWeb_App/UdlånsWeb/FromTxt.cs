44fb271dd877902a2773f07740d6dcb5ee6cc88e
Martin Madsen <martin_riehn_madsen@hotmail.com>
2020-08-10 08:42:48 +0200

    read from file fixed

--- UdlånsWeb_App/UdlånsWeb/FromTxt.cs
+++ UdlånsWeb_App/UdlånsWeb/FromTxt.cs
@@ -9,72 +9,14 @@ namespace UdlånsWeb
 {
     class FromTxt
     {
-        OpenFileDialog fileDialog = new OpenFileDialog();
-
-        public string StringFromTxt()
+        public string[] StringsFromTxt(string path)
         {
-            // lets user select the file to read
-            fileDialog.ShowDialog();
-
-            if (fileDialog.FileName != null && fileDialog.FileName != string.Empty)
+            try
             {
-                // reads file into result string
-                string result = File.ReadAllText(fileDialog.FileName);
-
-                // resets the file dialog
-                fileDialog.FileName = string.Empty;
-
-                // returns result string
+                string[] result = File.ReadAllLines(path);
                 return result;
             }
-            else
-            {
-                return null;
-            }
-        }
-
-        public string[] StringsFromTxt()
-        {
-            // lets user select the file to read
-            fileDialog.ShowDialog();
-
-            if (fileDialog.FileName != null && fileDialog.FileName != string.Empty)
-            {
-                // reads file into result array
-                string[] result = File.ReadAllLines(fileDialog.FileName);
-
-                // resets the filedialog
-                fileDialog.FileName = string.Empty;
-
-                // returns result array
-                return result;
-            }
-            else
-            {
-                return null;
-            }
-        }
-
-        public char[] CharsFromTxt()
-        {
-            // lets user select the file to read
-            fileDialog.ShowDialog();
-
-            if (fileDialog.FileName != null && fileDialog.FileName != string.Empty)
-            {
-                // reads file into string text
-                string text = File.ReadAllText(fileDialog.FileName);
-
-                // converts string text to result char array
-                char[] result = text.ToCharArray();
-
-                // resets the filedialog
-                fileDialog.FileName = string.Empty;
-
-                // returns result char array
-                return result;
-            }
-            else
+            catch (Exception e)
             {
                 return null;
             }
