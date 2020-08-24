16092db952dddfddbedd4e0a832e360ccff407ec
jesper Klejs Deleurand <Jesperd55@gmail.com>
2020-08-24 14:15:08 +0200

    Started on login logic

---
+++ UdlånsWeb_App/UdlånsWeb/DataHandling/ConvertLoginData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UdlånsWeb.Models;
+
+namespace UdlånsWeb.DataHandling
+{
+    public class ConvertLoginData
+    {
+        ConvertUserData convertuserdata = new ConvertUserData();
+        public bool loginConfirmed = false;
+
+        public void CheckLogin(String input)
+        {
+            try
+            {
+                foreach(User user in convertuserdata.GetUsers().Users)
+                {
+                    if(user.Initials == input)
+                    {
+                        Console.WriteLine("Access granted");
+                        loginConfirmed = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Access denied");
+                        loginConfirmed = false;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Login method failed");
+            }
+        }
+
+
+    }
+}
