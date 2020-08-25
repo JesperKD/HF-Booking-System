62372eaa1ae4e27e04ea904af25592bf95718d68
Kean <50844146+Mojiez@users.noreply.github.com>
2020-08-25 09:41:25 +0200

    changed discription on course duration

--- UdlånsWeb_App/UdlånsWeb/Models/Course.cs
+++ UdlånsWeb_App/UdlånsWeb/Models/Course.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ namespace UdlånsWeb.Models
     {
         [DisplayName("Fag")]
         public string Name { get; set; }
+        [DisplayName("Antal elever")]
         public int NumberOfStudents { get; set; }
         [DisplayName("Fagets varighed i dage, weekend skal inkluderes")]
         public int Duration { get; set; }
