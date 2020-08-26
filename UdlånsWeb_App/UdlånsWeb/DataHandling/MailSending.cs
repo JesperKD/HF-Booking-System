aab395deeefc7e11466c6f5d16f69d2982a185cd
Martin Madsen <martin_riehn_madsen@hotmail.com>
2020-08-26 10:04:22 +0200

    Create MailSending.cs

---
+++ UdlånsWeb_App/UdlånsWeb/DataHandling/MailSending.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace UdlånsWeb.DataHandling
+{
+    public class MailSending
+    {
+        public static void Email(string htmlString)
+        {
+            // rewrite to zbc mail instead
+            // rewrite to handle multiple admins
+            try
+            {
+                MailMessage message = new MailMessage();
+                SmtpClient smtp = new SmtpClient();
+                message.From = new MailAddress("FromMailAddress");
+                message.To.Add(new MailAddress("ToMailAddress"));
+                message.Subject = "Lån af host";
+                message.IsBodyHtml = true; //to make message body as html  
+                message.Body = htmlString;
+                smtp.Port = 587;
+                smtp.Host = "smtp.gmail.com"; //for gmail host  
+                smtp.EnableSsl = true;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new NetworkCredential("FromMailAddress", "password"); //Credentials to login to email 
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.Send(message);
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
