49aeb53030e2739b4c248991186dc6da375e1633
Kean <50844146+Mojiez@users.noreply.github.com>
2020-08-27 13:58:19 +0200

    Added controllers

---
+++ UdlånsWeb_App/UdlånsWeb/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UdlånsWeb.Controllers
+{
+    public class UserController : Controller
+    {
+       
+        // add all methods with User tag
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
