d26ea8d14a82bb837321898b9b6d2d33e65b427e
jesper Klejs Deleurand <Jesperd55@gmail.com>
2020-08-27 08:35:16 +0200

    Update ItemController.cs

--- UdlånsWeb_App/UdlånsWeb/Controllers/ItemController.cs
+++ UdlånsWeb_App/UdlånsWeb/Controllers/ItemController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using UdlånsWeb.DataHandling;
 using UdlånsWeb.Models;
 
@@ -14,25 +15,37 @@ namespace UdlånsWeb.Controllers
     [ApiController]
     public class ItemController : ControllerBase
     {
-        ConvertItemData data = new ConvertItemData();
-        ItemViewModel model = new ItemViewModel();
+        //ConvertItemData data = new ConvertItemData();
+        //ItemViewModel model = new ItemViewModel();
+        //public itemcontroller()
+        //{
+
+        //    model.items = data.getitems().items;
+        //}
+
+        ConvertUserData ConvertUserData = new ConvertUserData();
+        public User currentUser;
+
         public ItemController()
         {
-            model.Items = data.GetItems().Items;
+            currentUser = new User();
+            currentUser.Name = Environment.UserName;
         }
 
         // GET: api/<ItemController>
         [HttpGet]
-        public ItemViewModel Get()
+        public User Get()
         {
-            return model;
+            //return model;
+            return currentUser;
         }
 
         // GET api/<ItemController>/5
         [HttpGet("{id}")]
         public Item Get(int id)
         {
-            return model.Items.Where(x => x.Id == id).FirstOrDefault();
+            //return model.Items.Where(x => x.Id == id).FirstOrDefault();
+            return null;
         }
 
         // POST api/<ItemController>
