1140d2efbb53d077fe0ede53f07595d5f1971f6e
jesper Klejs Deleurand <Jesperd55@gmail.com>
2020-09-02 13:24:20 +0200

    Updated ConvertLoginData.cs

--- UdlånsWeb_App/UdlånsWeb/DataHandling/ConvertLoginData.cs
+++ UdlånsWeb_App/UdlånsWeb/DataHandling/ConvertLoginData.cs
@@ -20,30 +20,6 @@ namespace UdlånsWeb.DataHandling
         ConvertUserData convertuserdata = new ConvertUserData();
         public bool loginConfirmed = false;
 
-        public void CheckLogin(String input)
-        {
-            try
-            {
-                foreach (User user in convertuserdata.GetUsers().Users)
-                {
-                    if (user.Initials == input)
-                    {
-                        Console.WriteLine("Access granted");
-                        loginConfirmed = true;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Access denied");
-                        loginConfirmed = false;
-                    }
-                }
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Login method failed");
-            }
-        }
-
         public User AutoLogin()
         {
             User user = new User();
@@ -55,9 +31,9 @@ namespace UdlånsWeb.DataHandling
 
             try
             {
-                if (userModel.Users.Any(x => x.Initials == iniSplit[0].ToUpper()))
+                if (userModel.Users.Any(x => x.Initials == iniSplit[0].ToUpper() && x.Password == user.Password))
                 {
-                    user = userModel.Users.Where(x => x.Initials == iniSplit[0].ToUpper()).FirstOrDefault();
+                    user = userModel.Users.Where(x => x.Initials == iniSplit[0].ToUpper() && x.Password == user.Password).FirstOrDefault();
                     return user;
                 }
             }
@@ -76,9 +52,9 @@ namespace UdlånsWeb.DataHandling
             UserViewModel userModel = convertuserdata.GetUsers();
             try
             {
-                if (userModel.Users.Any(x => x.Initials == Initials.ToUpper()))
+                if (userModel.Users.Any(x => x.Initials == Initials.ToUpper() && x.Password == user.Password))
                 {
-                    user = userModel.Users.Where(x => x.Initials == Initials.ToUpper()).FirstOrDefault();
+                    user = userModel.Users.Where(x => x.Initials.ToUpper() == user.Initials.ToUpper() && x.Password == user.Password).FirstOrDefault();
                     return user;
                 }
             }
