dc7f6bce2e2f7c9950329e1d9c9b4d50eac420ec
Martin Madsen <martin_riehn_madsen@hotmail.com>
2020-09-17 11:03:46 +0200

    Update ConvertLoginData.cs
    code clean up

--- UdlånsWeb_App/UdlånsWeb/DataHandling/ConvertLoginData.cs
+++ UdlånsWeb_App/UdlånsWeb/DataHandling/ConvertLoginData.cs
@@ -21,55 +21,6 @@ namespace UdlånsWeb.DataHandling
         ConvertUserData convertuserdata = new ConvertUserData();
         public bool loginConfirmed = false;
 
-        public void CheckLogin(String input)
-        {
-            try
-            {
-                foreach (User user in convertuserdata.GetUsers().Users)
-                {
-                    if (user.Initials == input)
-                    {
-                        Console.WriteLine("Access granted");
-                        loginConfirmed = true;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Access denied");
-                        loginConfirmed = false;
-                    }
-                }
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Login method failed");
-            }
-        }
-
-        public User AutoLogin()
-        {
-            User user = new User();
-
-            string initials = Environment.UserName;
-            string[] iniSplit = initials.Split('.');
-
-            UserViewModel userModel = convertuserdata.GetUsers();
-
-            try
-            {
-                if (userModel.Users.Any(x => x.Initials == iniSplit[0].ToUpper()))
-                {
-                    user = userModel.Users.Where(x => x.Initials == iniSplit[0].ToUpper()).FirstOrDefault();
-                    return user;
-                }
-            }
-            catch (Exception)
-            {
-                return null;
-            }
-            return null;
-
-        }
-
         public User ManuelLogin(string Initials, string pass)
         {
             User user = new User();
